b8e38c94607acb3d701c93461fc0638d4e7cec0c
Rounak Singh <rounaksingh@Rounaks-MacBook-Air.local>
2021-12-19 02:23:57 +0530

    Added landing page, images & routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,25 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { JobsComponent } from './jobs/jobs.component';
+import { HomeComponent } from './home/home.component';
+
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'jobs', component: JobsComponent },
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    JobsComponent
+    JobsComponent,
+    HomeComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes),
   ],
   providers: [],
   bootstrap: [AppComponent]
