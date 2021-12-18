f30b3576d7a2324119a9897615e4c9e6a86cd9c6
Rounak Singh <rounaksingh@Rounaks-MacBook-Air.local>
2021-12-19 01:51:48 +0530

    Added navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'the-pm-door';
+  jobs = [1,2,3,4,5,6]
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { JobsComponent } from './jobs/jobs.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    JobsComponent
   ],
   imports: [
     BrowserModule
