4539012503854b7a204eee7616f1ae332d2677f9
Rounak Singh <rounaksingh@Rounaks-MacBook-Air.local>
2022-01-03 12:46:01 +0530

    Logo added, font change

--- src/app/jobs/jobs.component.ts
+++ src/app/jobs/jobs.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class JobsComponent implements OnInit {
   
-  jobs = [1,2,3,4,5,6]
+  jobs = [1,2,3,4,5,6, 7, 8, 9, 10, 1,2,3,4,5,6, 7, 8, 9, 10, 1,2,3,4,5,6, 7, 8, 9, 10, 1,2,3,4,5,6, 7, 8, 9, 10, 1,2,3,4,5,6, 7, 8, 9, 10]
 
   constructor() { }
 
